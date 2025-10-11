74802e2bcd9ba6accf2c7974831db7ef26a7525a
Okerew <93822247+Okerew@users.noreply.github.com>
2025-10-11 10:39:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,25 +7,36 @@ curl -L https://sh.distant.dev | sh
 
 brew tap homebrew-zathura/zathura
 brew install zathura
-brew install cargo-outdated
-brew install cargo-audit
 
 brew install switchaudio-osx
 brew install nowplaying-cli
+
+brew tap FelixKratz/formulae
+brew install sketchybar
+brew install borders
 
 brew install --cask sf-symbols
 brew install --cask font-sf-mono
 brew install --cask font-sf-pro
 
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+
+(git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
+
 echo "Cloning Config"
 git clone https://github.com/Okerew/dotfiles /tmp/dotfiles
 
-mkdir -p ~/.config/nvim
-mv /tmp/dotfiles/nvim/* ~/.config/nvim/
+mv $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
 
-python3 -m venv ~/.local/venv/nvim
-source ~/.local/venv/nvim/bin/activate
-pip install pynvim
+BORDERS_PATH="$(brew --prefix borders)/share/borders"
+mkdir -p $HOME/.config/borders
+cp -r "$BORDERS_PATH"/* $HOME/.config/borders/
+
+mv /tmp/dotfiles/borders $HOME/.config/borders
+mv /tmp/dotfiles/sketchybar $HOME/.config/sketchybar
+
+mkdir -p ~/.nvim/config
+mv /tmp/dotfiles/nvim/* ~/.nvim/config/
 
 echo "Installing tmux"
 brew install tmux
@@ -36,3 +47,6 @@ mv /tmp/dotfiles/tmux/* $HOME/.config/tmux/
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 rm -rf /tmp/dotfiles
+
+brew services start sketchybar
+brew services start borders
